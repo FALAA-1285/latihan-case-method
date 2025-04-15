7106f13784dc126515e45e2acc46bd9d30132f59
FALAA-1285 <ahmadfalahi1285@gmail.com>
2025-04-15 18:43:47 +0700

    atribute dan kontruktor

--- hewan.java
+++ hewan.java
@@ -1,3 +1,16 @@
 public class hewan{
-    
+    String id;
+    String nama;
+    String jenis;
+    int umur;
+
+    hewan(){
+
+    }
+    hewan(String id, String nama, String jenis, int umur){
+        this.id=id;
+        this.nama=nama;
+        this.jenis=jenis;
+        this.umur=umur;
+    }
 }
