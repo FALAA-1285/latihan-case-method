f6de913531688ae165dd097aa501c4813b1ebfd6
FALAA-1285 <ahmadfalahi1285@gmail.com>
2025-04-15 19:07:43 +0700

    tambah method tambah data dan tampildata

--- dataHewan.java
+++ dataHewan.java
@@ -1,3 +1,23 @@
 public class dataHewan {
+    hewan[] hewans=new hewan[100];
+    int idx=0;
     
+    public void TambahData(hewan hwn){
+        if(idx<hewans.length){
+            hewans[idx++]=hwn;
+        }else{
+            System.out.println("Data sudah penuh");
+        }
+    }
+    public void tampilkanData(){
+        for(int i=0; i<idx-1; i++){
+            if(idx==0){
+                System.out.println("Data hewan belum dimasukkan");
+            }else{
+                for (int j = 0; j < idx-1; j++) {
+                    hewans[i].tampil();
+                }
+            }
+        }
+    }
 }
